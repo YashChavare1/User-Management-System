0d55b67cafe8dbccab11688fa76dc72fe22d08e5
Yash <yashchavare1@gmail.com>
2024-08-26 12:06:09 +0530

    UserId and DOB Sorting Filter Created

---
+++ frontend/src/Components/User List Navbar/UserListNavbar.js
@@ -0,0 +1,91 @@
+import { useState } from "react";
+import "./UserListNavbar.css"
+
+export const UserListNavbar = ({ userData, setDisplayUsers }) => {
+    const [data, setData] = useState(userData);
+    const [isFilterVisible, setFilterVisible] = useState(false);
+
+    const handleSortUserId = (event) => {
+        const { value } = event.target;
+
+        if (value === "") {
+            console.log("return");
+            return;
+        }
+
+        const sorted = [...data].sort((a, b) => {
+            if (value === "ascending") {
+                return a.userId - b.userId;
+            }
+
+            if (value === "descending") {
+                return b.userId - a.userId;
+            }
+            return 0;
+        });
+
+        setDisplayUsers(sorted);
+        console.log("Sorted by UserId:", sorted);
+    };
+
+
+    const handleSortDOB = (event) => {
+        const { value } = event.target;
+
+        if (value === "") {
+            console.log("return");
+            return;
+        }
+
+        const sorted = [...data].sort((a, b) => {
+            const dateA = new Date(a.dateOfBirth);
+            const dateB = new Date(b.dateOfBirth);
+
+            if (value === "ascending") {
+                return dateB - dateA;
+            }
+            if (value === "descending") {
+                return dateA - dateB;
+            }
+            return 0;
+        });
+
+        setDisplayUsers(sorted);
+        console.log(sorted);
+    };
+
+    return (
+        <>
+            <div className="filter-button">
+                <button onClick={() => setFilterVisible(!isFilterVisible)}>
+                    {isFilterVisible ? "Hide Filters" : "Filters"}
+                </button>
+            </div>
+
+            <div className="user-list-navbar">
+                {isFilterVisible && (
+                    <div className="sort-options">
+                        <div className="sort-userId">
+                            <h3>User Id</h3>
+                            <select name="sort" id="sort" onChange={handleSortUserId}>
+                                <option value="">-- Select -- </option>
+                                <option value="ascending">Ascending</option>
+                                <option value="descending">Descending</option>
+                            </select>
+                        </div>
+
+
+                        <div className="sort-dob">
+                            <h3>Date of Birth</h3>
+                            <select name="sort" id="sort" onChange={handleSortDOB}>
+                                <option value="">-- Select -- </option>
+                                <option value="ascending">Ascending</option>
+                                <option value="descending">Descending</option>
+                            </select>
+                        </div>
+                    </div>
+                )}
+            </div>
+        </>
+    );
+};
--- frontend/src/Components/UserList/UserList.js
+++ frontend/src/Components/UserList/UserList.js
@@ -1,9 +1,16 @@
 import { UserCard } from "../UserCard/UserCard";
 import { useGetUser } from "../../Hooks/useGetUser";
 import "./UserList.css";
+import { UserListNavbar } from "../User List Navbar/UserListNavbar";
+import { useEffect, useState } from "react";
 
 export const UserList = () => {
     const { userData, loading } = useGetUser();
+    const [displayUser, setDisplayUsers] = useState([]);
+
+    useEffect(() => {
+        setDisplayUsers(userData);
+    }, [userData]);
 
     if (loading) {
         return <div className="loader">Loading...</div>;
@@ -12,9 +19,10 @@ export const UserList = () => {
     return (
         <div className="card-list">
             <h1>User List</h1>
-            {userData.length !== 0 ? <div className="cards">
+            { displayUser.length > 0 && <UserListNavbar userData={userData} setDisplayUsers={setDisplayUsers} />}
+            {displayUser.length !== 0 ? <div className="cards">
                 {
-                    userData.map((user) => (
+                    displayUser.map((user) => (
                         <UserCard userData={user} key={user.userId} />
                     ))
                 }
